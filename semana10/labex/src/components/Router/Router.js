930bfdb61bf2025ccdbde1af023fa69661b30287
Kelly Knoblauch <kellyknoblauch@MacBook-Pro-de-Kelly.local>
2020-11-17 17:19:18 -0300

    add router

--- semana10/labex/src/components/Router/Router.js
+++ semana10/labex/src/components/Router/Router.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Link } from "react-router-dom";
  
-// using CommonJS modules
+
 const BrowserRouter = require("react-router-dom").BrowserRouter;
 const Route = require("react-router-dom").Route;
 const Link = require("react-router-dom").Link;
