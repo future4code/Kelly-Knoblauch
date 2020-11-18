5dd2f2d5d333be1f8e1c182ea293ba941877d4f0
Kelly Knoblauch <kellyknoblauch@MacBook-Pro-de-Kelly.local>
2020-11-18 16:25:17 -0300

    aprimorando organizaçao de rotas

--- semana10/labex/src/components/Router/Router.js
+++
@@ -1,6 +0,0 @@
-import { BrowserRouter, Route, Link } from "react-router-dom";
- 
-
-const BrowserRouter = require("react-router-dom").BrowserRouter;
-const Route = require("react-router-dom").Route;
-const Link = require("react-router-dom").Link;
